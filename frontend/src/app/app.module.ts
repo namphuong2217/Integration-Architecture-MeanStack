5131fc56e525747f42c3ac0629f7bde6f71392bb
smnschmnck <simon.schmeinck@gmx.de>
2022-01-17 17:38:02 +0100

    added simple register component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppRouting } from './app.routing';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { LoginComponent } from './components/login/login.component';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { RegisterComponent } from './components/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
@@ -22,13 +23,13 @@ import { MatSortModule } from '@angular/material/sort';
 import { OrderEvaluationComponent } from './components/order-evaluation/order-evaluation.component';
 import { SocialPerformanceComponent } from './components/social-performance/social-performance.component';
 import { EnterSocialPerformancePageComponent } from './pages/enter-social-performance-page/enter-social-performance-page.component';
-import { ReactiveFormsModule  } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BonusComputationCollectionPageComponent } from './pages/bonus-computation-collection-page/bonus-computation-collection-page.component';
 import { SocialPerformanceFormComponent } from './components/social-performance-form/social-performance-form.component';
 import { EmployeeAndYearSelectorComponent } from './components/employee-and-year-selector/employee-and-year-selector.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from "@angular/material/select";
-import {MatListModule} from "@angular/material/list";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import {MatListModule} from "@angular/material/list";
     EnterSocialPerformancePageComponent,
     BonusComputationCollectionPageComponent,
     SocialPerformanceFormComponent,
-    EmployeeAndYearSelectorComponent
+    EmployeeAndYearSelectorComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
@@ -62,9 +64,9 @@ import {MatListModule} from "@angular/material/list";
     ReactiveFormsModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatListModule
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
