6a55fb4408ca5193c8b006ea79471c72e305aca6
smnschmnck <simon.schmeinck@gmx.de>
2022-01-19 16:13:07 +0100

    changed code styling

--- backend/src/models/BonusCompCollection.js
+++ backend/src/models/BonusCompCollection.js
@@ -1,7 +1,7 @@
 
 class BonusCompCollection {
-    constructor(sid, year, salesman, orderEvaluation, socialPerformance, approvedByCEO=false,
-                approvedByHR = false, bonusOrder = 0, bonusSocial = 0) {
+    constructor(sid, year, salesman, orderEvaluation, socialPerformance, approvedByCEO = false,
+        approvedByHR = false, bonusOrder = 0, bonusSocial = 0) {
         this.sid = sid;
         this.year = year;
         this.salesman = salesman;
