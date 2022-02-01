f18694a59c2267d1e10debeb43117cca56484cbb
twagne2s <tim.wagner@smail.inf.h-brs.de>
2022-02-01 20:04:21 +0100

    Outsourcing and separation of authentication in the backend - use of middleware with function input

--- backend/src/Globals.js
+++ backend/src/Globals.js
@@ -1,22 +1,4 @@
 
-const departments = {
-    sales : "Sales",
-    hr : "HR",
-    ceo : "Leader"
-}
-
-exports.hasRoleHR = function(user) {
-    return user.role == departments.hr;
-}
-
-exports.hasRoleCEO = function (user){
-    return user.role == departments.ceo;
-}
-
-exports.hasRoleSales = function (user){
-    return user.role == departments.sales;
-}
-
 exports.Permissions ={
     sales : "Sales",
     hr : "HR",
--- backend/src/middlewares/auth-middleware.js
+++ backend/src/middlewares/auth-middleware.js
@@ -1,17 +1,24 @@
+const Permissions = require("../Globals").Permissions;
+
 /**
  * this express middleware checks if a user is authenticated or even has admin permissions;
  * otherwise the request gets intercepted and status 401 is returned
  * @param {boolean} beAdmin if true, user needs to be admin
  * @return {(function(*, *, *): void)|*}
  */
-exports.checkAuthorization = (beAdmin) => {
+exports.checkAuthorization = (action, beAdmin) => {
     return (req, res, next) => {
-        if(req.session.authenticated){ //check if session was marked as authenticated
-            if(!beAdmin || req.session.user.isAdmin){ //check if admin-requirement is met
-                next(); //proceed with next middleware or handler
-                return;
+        if(req.session.authenticated){
+            if(true || Permissions.hasUserPermission(req.session.user, action)){ //TODO delete true and adapt hasUserPermission
+                if(!beAdmin || req.session.user.isAdmin){
+                    next();
+                    return;
+                }
             }
         }
-        res.status(401).send(); //intercept request
+        res.status(401).send("no permission"); //intercept request
     }
 }
+
+//in api-route.js
+//router.get("/....path", checkAuthorization("postBonus"), api.function);
