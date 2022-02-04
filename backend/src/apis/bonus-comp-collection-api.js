0318f6215fcd786b1d7192ebe45f73fc71d03979
smnschmnck <simon.schmeinck@gmx.de>
2022-02-04 12:35:47 +0100

    throw error on incomplete social performance for bonus

--- backend/src/apis/bonus-comp-collection-api.js
+++ backend/src/apis/bonus-comp-collection-api.js
@@ -1,25 +1,30 @@
 const bonusCompCollectionController = require("../controllers/bonus-comp-collection-controller");
 const Permissions = require("../Globals").Permissions;
 
-exports.getBonusCompCollection = async function(req, res) {
+exports.getBonusCompCollection = async function (req, res) {
     const sid = req.params.sid;
     const year = req.params.year;
     const db = req.app.get('db');
     const resp = await bonusCompCollectionController.getBonusComputationCollection(sid, year, db);
+    if (resp === "error") {
+        res.status(404);
+        return res.send("incomplete social perforamnce");
+    }
+    console.log(resp);
     return res.send(resp);
 }
 
 
-exports.postBonusCompCollection = async function(req, res){
+exports.postBonusCompCollection = async function (req, res) {
     const body = req.body;
     const db = req.app.get('db');
     const user = req.session.user; //{role : "HR"};//
-    let resp ;
-    if(Permissions.hasUserPermission(user, "approveBonusHR")){
+    let resp;
+    if (Permissions.hasUserPermission(user, "approveBonusHR")) {
         resp = await bonusCompCollectionController.approvedByHR(body.sid, body.year, db);
-    } else if(Permissions.hasUserPermission(user, "approveBonusCEO")){
+    } else if (Permissions.hasUserPermission(user, "approveBonusCEO")) {
         resp = await bonusCompCollectionController.approvedByCEO(body.sid, body.year, body.comments, db);
     }
-    if(!resp){res.send({status: 400, msg: "permission error"})}
+    if (!resp) { res.send({ status: 400, msg: "permission error" }) }
     return res.send(resp);
 }
--- backend/src/controllers/bonus-comp-collection-controller.js
+++ backend/src/controllers/bonus-comp-collection-controller.js
@@ -8,90 +8,92 @@ const socialPerformanceTargetService = require("../services/social-performance-t
 
 const socialPerformanceService = require("../services/social-performance-service");
 const bonusCalcEnricher = require("./transformation/bonus-calculation-enricher");
-const {translateRatingToString} = require("./transformation/order-evaluation-ranking-translator");
+const { translateRatingToString } = require("./transformation/order-evaluation-ranking-translator");
 
-const getBonusComputationCollection = async function(sid, year, db) {
-    year=parseInt(year);
+const getBonusComputationCollection = async function (sid, year, db) {
+    year = parseInt(year);
     // If the collection is already in database
     const bonusCompCollectionResp = await bonusCompCollectionService.readBonusCompCollection(sid, year, db);
-    if(bonusCompCollectionResp.status === 200){
+    if (bonusCompCollectionResp.status === 200) {
         return bonusCompCollectionResp.payload;
     }
 
     // Collect data from different controllers
     const orderEvaluation = await orderEvaluationController.getOrderEvaluations(sid, year);
     const socialPerformance = await socialPerformanceService.getYearAverage(db, sid, year);
+    if (socialPerformance.status === "error") return "error";
     const salesman = await salesmanController.getEmployee(sid);
 
-    const bonusOrder=[];
+    const bonusOrder = [];
     orderEvaluation.forEach(orderEval => {
         bonusOrder.push(bonusCalcEnricher.getBonusForSale(orderEval.nameProduct, orderEval.clientRanking, orderEval.items));
         orderEval.clientRanking = translateRatingToString(orderEval.clientRanking);
     });
 
     const targetResp = await socialPerformanceTargetService.get(db, sid, year);
-    const bonusSocial=[];
-    if(targetResp.status === 200){
-        Object.keys(socialPerformance).filter(key => key !== "sid" && key !=="year").forEach(socialKey =>{
+    if (targetResp === "no targets for sid") return "error";
+    const bonusSocial = [];
+    if (targetResp.status === 200) {
+        Object.keys(socialPerformance).filter(key => key !== "sid" && key !== "year").forEach(socialKey => {
             bonusSocial.push(bonusCalcEnricher.getBonusForSocialPerformance(socialKey, targetResp.payload[socialKey], socialPerformance[socialKey]));
         });
     }
 
     return new BonusCompCollection(sid, year, salesman, orderEvaluation,
-        socialPerformance,false, false, bonusOrder, bonusSocial, targetResp.payload);
+        socialPerformance, false, false, bonusOrder, bonusSocial, targetResp.payload);
 }
 
 module.exports.getBonusComputationCollection = getBonusComputationCollection;
 
-exports.approvedByCEO = async function(sid, year, comments, db){
+exports.approvedByCEO = async function (sid, year, comments, db) {
     const bonusCompCollectionResp = await bonusCompCollectionService.readBonusCompCollection(sid, year, db);
     //if not yet in database
-    if(bonusCompCollectionResp.status !== 200){
+    if (bonusCompCollectionResp.status !== 200) {
         let bonusCompCollection = await getBonusComputationCollection(sid, year, db);
         bonusCompCollection.approvedByCEO = true;
         bonusCompCollection.comments = comments;
-        if(!hasCommentArraySameLength(bonusCompCollection.orderEvaluation, bonusCompCollection.socialPerformance, comments)){
-            return {status:400, msg: "wrong input comment"}
+        if (!hasCommentArraySameLength(bonusCompCollection.orderEvaluation, bonusCompCollection.socialPerformance, comments)) {
+            return { status: 400, msg: "wrong input comment" }
         }
         return await bonusCompCollectionService.writeBonusCompCollection(bonusCompCollection, db);
     }
-    if(!hasCommentArraySameLength(bonusCompCollectionResp.payload.orderEvaluation, bonusCompCollectionResp.payload.socialPerformance, comments)){
-        return {status:400, msg: "wrong input comment"}
+    if (!hasCommentArraySameLength(bonusCompCollectionResp.payload.orderEvaluation, bonusCompCollectionResp.payload.socialPerformance, comments)) {
+        return { status: 400, msg: "wrong input comment" }
     }
     //if already approved
-    if(bonusCompCollectionResp.payload.approvedByCEO){
-        return JSON.stringify({status: "404", msg: `collection is already approved`});
+    if (bonusCompCollectionResp.payload.approvedByCEO) {
+        return JSON.stringify({ status: "404", msg: `collection is already approved` });
     }
     //if both approved - send to orangehrm
-    if(bonusCompCollectionResp.payload.approvedByHR){
+    if (bonusCompCollectionResp.payload.approvedByHR) {
         await salesmanController.postEmployeeBonus(sid, year, bonusCompCollectionResp.payload.bonusTotal);
     }
     //update bonusCompCollection
     return await bonusCompCollectionService.updateBonusCompCollection(
-        sid, year, {"approvedByCEO" : true, comments : comments}, db);
+        sid, year, { "approvedByCEO": true, comments: comments }, db);
 }
 
-exports.approvedByHR = async function(sid, year, db){
+exports.approvedByHR = async function (sid, year, db) {
     const bonusCompCollectionResp = await bonusCompCollectionService.readBonusCompCollection(sid, year, db);
     //if not yet in database
-    if(bonusCompCollectionResp.status !== 200){
+    if (bonusCompCollectionResp.status !== 200) {
         let bonusCompCollection = await getBonusComputationCollection(sid, year, db);
         bonusCompCollection.approvedByHR = true;
         return await bonusCompCollectionService.writeBonusCompCollection(bonusCompCollection, db);
     }
     //if already approved
-    if(bonusCompCollectionResp.payload.approvedByHR){
-        return JSON.stringify({status: "404", msg: `collection is already approved`});
+    if (bonusCompCollectionResp.payload.approvedByHR) {
+        return JSON.stringify({ status: "404", msg: `collection is already approved` });
     }
     //if both approved - send to orangehrm
-    if(bonusCompCollectionResp.payload.approvedByCEO){
+    if (bonusCompCollectionResp.payload.approvedByCEO) {
         await salesmanController.postEmployeeBonus(sid, year, bonusCompCollectionResp.bonusTotal); // post bonus on OHRM
     }
     //update bonusCompCollection
-    return await bonusCompCollectionService.updateBonusCompCollection(sid, year, {"approvedByHR" : true}, db);
+    return await bonusCompCollectionService.updateBonusCompCollection(sid, year, { "approvedByHR": true }, db);
 }
 
 //HELPER FUNCTION
-function hasCommentArraySameLength(salesOrder, socialperformance,  comments){
+function hasCommentArraySameLength(salesOrder, socialperformance, comments) {
     return comments.length === salesOrder.length + (socialperformance.integrity ? 6 : 0);
 }
