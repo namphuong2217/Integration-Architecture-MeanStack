88ce9ff9d3ec0d485f304881f4a1055120d15c32
smnschmnck <simon.schmeinck@gmx.de>
2022-02-04 12:58:22 +0100

    fixed wrong switch cases

--- backend/src/controllers/transformation/bonus-calculation-enricher.js
+++ backend/src/controllers/transformation/bonus-calculation-enricher.js
@@ -1,40 +1,40 @@
 
 
-exports.getBonusForSale = (productName, ranking, items) =>{
+exports.getBonusForSale = (productName, ranking, items) => {
     ranking = Number(ranking);
     items = Number(items);
-    switch(productName){
-        case("HooverClean"):
-            return (7-ranking)*items*10;
-        case("HooverGo"):
-            return (7-ranking)*items*5;
+    switch (productName) {
+        case ("HooverClean"):
+            return (7 - ranking) * items * 10;
+        case ("HooverGo"):
+            return (7 - ranking) * items * 5;
         default:
-            return (7-ranking)*items;
+            return (7 - ranking) * items;
     }
 }
 
 
-exports.getBonusForSocialPerformance = (performance, target, actual) =>{
+exports.getBonusForSocialPerformance = (performance, target, actual) => {
     const factorDiff = factorDiffTargAct(target, actual);
-    switch(performance){
-        case("leadershipCompetence"):
-            return factorDiff*target*5;
-        case("openness"):
-            return factorDiff*target*5;
-        case("socialBehaviour"):
-            return factorDiff*target*10;
-        case("attitude"):
-            return factorDiff*target*5;
-        case("communicationSkills"):
-            return factorDiff*target*8;
-        case("integrity"):
-            return factorDiff*target*8;
+    switch (performance) {
+        case ("leadership_competence"):
+            return factorDiff * target * 5;
+        case ("openness"):
+            return factorDiff * target * 5;
+        case ("social_behaviour"):
+            return factorDiff * target * 10;
+        case ("attitude"):
+            return factorDiff * target * 5;
+        case ("comm_skills"):
+            return factorDiff * target * 8;
+        case ("integrity"):
+            return factorDiff * target * 8;
         default:
             return null;
     }
 }
 
-const factorDiffTargAct = (target, actual) =>{
+const factorDiffTargAct = (target, actual) => {
     const diff = actual - target;
-    return 1.5 * Math.exp(diff*0.5);
+    return 1.5 * Math.exp(diff * 0.5);
 }
