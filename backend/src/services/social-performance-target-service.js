620a72f0482f33f0dd1fbeca686a99cf8e6a195e
twagne2s <tim.wagner@smail.inf.h-brs.de>
2022-02-12 09:39:57 +0100

    Bug fix - year string from url must be read as int

--- backend/src/services/social-performance-target-service.js
+++ backend/src/services/social-performance-target-service.js
@@ -35,7 +35,7 @@ exports.targetsExist = async (db, year) => {
 exports.get = async (db, sid, year) => {
     //if (user.role !== "Leader") return { status: 401, payload: "only the ceo is allowed to perform this action" };
     let spTargetCollection = db.collection('socialPerformanceTargetCollection');
-    const filter = { sid: sid, year: year };
+    const filter = { sid: sid, year: Number(year) };
     const res = await spTargetCollection.findOne(filter);
     if (await res) return { status: 200, payload: await res };
     return { status: 404, payload: "no targets for sid" };
