6dd2ee7b732f749a957105294f1a7c59acd9f9ba
twagne2s <tim.wagner@smail.inf.h-brs.de>
2022-02-12 21:51:26 +0100

    order evaluation api with function get total sales

--- backend/src/apis/order-evaluation-api.js
+++ backend/src/apis/order-evaluation-api.js
@@ -1,11 +1,8 @@
 const orderEvaluationController = require("../controllers/order-evaluation-controller");
 
-exports.getOrderEvaluations = async function(req, res) {
-    const sid = req.params.sid;
+exports.getTotalSales = async function(req, res){
     const year = req.params.year;
-
-    const filteredOrderEvaluations = await orderEvaluationController.getOrderEvaluations(sid, year);
-
-    return res.send(filteredOrderEvaluations);
+    const resp = await orderEvaluationController.getNumberOfSales(year);
+    res.status(resp.status).send(resp.numberOfSales.toString());
 }
 
