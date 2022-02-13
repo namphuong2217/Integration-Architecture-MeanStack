b198427e9d6ba4f70267bdcf221bdfb84c589a2a
smnschmnck <simon.schmeinck@gmx.de>
2022-02-13 15:41:54 +0100

    fixed wrong param error. TODO: send correct req to hrm

--- backend/src/controllers/employee-controller.js
+++ backend/src/controllers/employee-controller.js
@@ -4,24 +4,24 @@ const bonusFilter = require("./transformation/bonus-filter");
 const Bonus = require("../models/Bonus")
 
 
-exports.getEmployee = async function(sid) {
+exports.getEmployee = async function (sid) {
     const resp = await salesManService.employeeRead(sid)
         .catch((error) => {
             console.log(error);
         });
-    if(resp.status !== 200){return resp}
+    if (resp.status !== 200) { return resp }
     return new Employee(resp.payload.code, resp.payload.firstName, resp.payload.lastName, resp.payload.unit);
 }
 
-exports.getSalesmen = async function() {
+exports.getSalesmen = async function () {
     const resp = await salesManService.employeesRead()
         .catch((error) => {
             console.log(error);
         });
-    if(resp.status !== 200){return resp}
+    if (resp.status !== 200) { return resp }
     let listOfSalesmenFinal = [];
-    for (const salesman of resp.payload){
-        if(salesman.unit === "Sales"){
+    for (const salesman of resp.payload) {
+        if (salesman.unit === "Sales") {
             const salesmanFinal = new Employee(salesman.code, salesman.firstName, salesman.lastName, salesman.unit);
             listOfSalesmenFinal.push(salesmanFinal);
         }
@@ -29,19 +29,20 @@ exports.getSalesmen = async function() {
     return listOfSalesmenFinal;
 }
 
-exports.getEmployeeBonus = async function(sid, year) {
+exports.getEmployeeBonus = async function (sid, year) {
     const resp = await salesManService.readEmployeeBonus(sid, year)
         .catch((error) => {
             console.log(error);
         });
-    if(resp.status !== 200){return resp}
+    if (resp.status !== 200) { return resp }
     return bonusFilter.filterOrderEvaluationByYear(resp.payload, year);
 }
 //bonus is updated if the value already exists
-exports.postEmployeeBonus = async function(sid, body) {
-    if(!sid || !body.year || !body.value){
-        return {status : 409, msg: "input error"};
+exports.postEmployeeBonus = async function (sid, year, bonus) {
+    console.log("HERE");
+    if (!sid || !year || !bonus) {
+        return { status: 409, msg: "input error" };
     }
-    const bonus = new Bonus(body.year, body.value);
-    return await salesManService.writeEmployeeBonus(sid, bonus);
+    const bonusObj = new Bonus(year, bonus);
+    return await salesManService.writeEmployeeBonus(sid, bonusObj);
 }
